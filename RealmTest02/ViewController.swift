bb822f7927c7baf412b850c5c150fc5720967679
Jonghyun Kim <jhkim@dit.ac.kr>
2019-06-02 17:43:15 +0900

    1. TableView 추가 2. cell 삭제 추가

--- RealmTest02/ViewController.swift
+++ RealmTest02/ViewController.swift
@@ -7,14 +7,95 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+// 데이터베이스 모델
+class Person: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var age = 0
+}
 
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var resultTableView: UITableView!
+    
+    // auto-updating container type in Realm returned from object queries.
+    // Results는 실시간으로 기본 데이터가 자동으로 뷰에 업데잍, 됨. 이는 Results가 다시 불러올 필요가 없다는 것을 말함
+    var personArray : Results<Person>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resultTableView.dataSource = self
+        
+        // 프로그램 실행 초기에 데이터 불러오기
+        let realm = try! Realm()
+        personArray = realm.objects(Person.self)
+        
+        // 데이터가 실제 저장된 디스크 위치 출력(Realm Broswer에서 사용) .../Documents/...
+        print(NSHomeDirectory())
+        
     }
+    
+    @IBAction func addObject(_ sender: Any) {
+        // 새객체생성
+        let newPerson = Person()
+        newPerson.name = nameTextField.text!
+        newPerson.age = Int(ageTextField.text!)!
+        
+        // DB에 저장
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(newPerson)
+        }
+        nameTextField.text = ""
+        ageTextField.text = ""
+        
+        // TableView의 데이터를 새로 갱신
+        resultTableView.reloadData()
+    }
+    
+    @IBAction func getObject(_ sender: Any) {
+        let realm = try! Realm()
+        personArray = realm.objects(Person.self).filter("age > 10")
+        print(personArray!)
 
-
+    }
+    
+    @IBAction func deleteObject(_ sender: Any) {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return personArray?.count ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = resultTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        cell.textLabel?.text = personArray![indexPath.row].name
+        cell.detailTextLabel?.text = String(personArray![indexPath.row].age)
+        return cell
+    }
+    
+    // cell delete
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let realm = try! Realm()
+            try! realm.write {
+                realm.delete(personArray![indexPath.row])
+            }
+            // 데이터 업데이트
+            personArray = realm.objects(Person.self)
+            self.resultTableView.reloadData()
+        }
+    }
 }
 
